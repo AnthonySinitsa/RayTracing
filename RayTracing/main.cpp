edea896eb4db1a9790ef0385cd2c894af688ee87
AnthonySinitsa <itonyemail@gmail.com>
2024-07-20 06:40:38 +0000

    updated fragment shader to render a sphere

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -51,7 +51,30 @@ std::string fragmentShader = R"fragmentShader(
 layout(location = 0) in vec2 fragUV;
 layout(location = 0) out vec4 outColor;
 void main() {
-	outColor = vec4(fragUV, 1.0, 1.0);
+	vec2 uv = (fragUV * 2.0) - 1.0;
+
+	// normalize and adjust for ratio
+	uv *= .8;
+
+	// initialize some colors
+	vec4 color1 = vec4(.4, .6, .7, 1.0);
+	vec4 color2 = vec4(.9, .7, .6, 1.0);
+
+	// shade with 2 faux lights
+	color1 *= .8 - distance(uv, vec2(-.1, -.1));
+	color2 *= .6 - distance(uv, vec2(.25, .3));
+	vec4 sphere = color1 + color2;
+
+	// limit edges to circle shape
+	float d = distance(uv, vec2(0.0));
+	// smooth edges
+	float t = 1.0 - smoothstep(.6, .61, d);
+	// apply shape to color
+	sphere *= t + 0.2 * uv.y;
+
+	// output final color and brighten
+	vec4 fragColor = sphere * 1.6;
+	outColor = vec4(fragColor.xyz, 1.0);
 }
 )fragmentShader";
 
