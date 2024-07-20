cfa6cf92f6d045a7ce3e1fb80c28f30fac48edd0
AnthonySinitsa <itonyemail@gmail.com>
2024-07-20 04:45:59 +0000

    bug fixing

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -8,14 +8,14 @@
 
 // inline shader
 #include <shaderc/shaderc.hpp>
-#pragma comment(lib, "shaderc.lib")
-#pragma comment(lib, "shaderc_utild.lib.lib")
+#pragma comment(lib, "shadercd.lib")
+#pragma comment(lib, "shaderc_utild.lib")
 #pragma comment(lib, "shaderc_combinedd.lib")
 
 //-----------------------------------
 //-----------------------------------
 
-std:string vertexShader = R"vertexshader(
+std::string vertexShader = R"vertexshader(
 
 #version 450
 #extension GL_ARB_seperate_shader_objects : enable
@@ -43,7 +43,7 @@ void main() {
 }
 )vertexshader";
 
-std:string fragmentShader = R"fragmentShader(
+std::string fragmentShader = R"fragmentShader(
 
 #version 450
 #extension GL_ARB_seperate_shader_objects : enable
@@ -68,7 +68,7 @@ int main() {
 	auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 	std::vector<const char*> glfwExtensionsVector(glfwExtensions, glfwExtensions + glfwExtensionCount);
 	const vk::ApplicationInfo applicationInfo("Hello World", 0, nullptr, 0, VK_API_VERSION_1_3);
-	const auto instance = vk::createInstanceUnique(vk::InsatnceCreateInfo({}, &applicationInfo, 0, nullptr, static_cast<uint32_t>(glfwExtensionsVector.size()), glfwExtensionsVector.data()));
+	const auto instance = vk::createInstanceUnique(vk::InstanceCreateInfo({}, &applicationInfo, 0, nullptr, static_cast<uint32_t>(glfwExtensionsVector.size()), glfwExtensionsVector.data()));
 
 	VkSurfaceKHR surfaceTmp;
 	VkResult err = glfwCreateWindowSurface(*instance, window, nullptr, &surfaceTmp);
@@ -80,7 +80,7 @@ int main() {
 
 	const auto physicalDevice = instance->enumeratePhysicalDevices()[0];
 
-	const std : vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+	const std::vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 	vk::UniqueDevice device = physicalDevice.createDeviceUnique(vk::DeviceCreateInfo(
 		vk::DeviceCreateFlags(), 1, &queueCreateInfos, 0u, nullptr, static_cast<uint32_t>(deviceExtensions.size()), deviceExtensions.data()
 	));
@@ -88,8 +88,7 @@ int main() {
 	uint32_t imageCount = 2;
 	auto format = vk::Format::eB8G8R8A8Unorm;
 
-	vk::SwapchainCreateInfoKHR swapChainCreateInfo({}, surface.get(), imageCount, format, vk::ColorSpaceKHR::eSrgbNonlinear, vk::Extent2D(width, height), 1, vk::ImageUsageFlagBits::eColorAttachment);
-	vk::SharingMode::eExclusive, 0u, static_cast<uint32_t*>(nullptr), vk::SurfaceTransformFlagBitsKHR::eIdentity, vk::CompositeAlphaFlagBitsKHR::eOpaque, vk::PresentModeKHR::eFifo, true, nullptr);
+	vk::SwapchainCreateInfoKHR swapChainCreateInfo({}, surface.get(), imageCount, format, vk::ColorSpaceKHR::eSrgbNonlinear, vk::Extent2D(width, height), 1, vk::ImageUsageFlagBits::eColorAttachment, vk::SharingMode::eExclusive, 0u, static_cast<uint32_t*>(nullptr), vk::SurfaceTransformFlagBitsKHR::eIdentity, vk::CompositeAlphaFlagBitsKHR::eOpaque, vk::PresentModeKHR::eFifo, true, nullptr);
 
 	auto swapChain = device->createSwapchainKHRUnique(swapChainCreateInfo);
 	std::vector<vk::Image> swapChainImages = device->getSwapchainImagesKHR(swapChain.get());
@@ -97,7 +96,7 @@ int main() {
 	std::vector<vk::UniqueImageView> imageViews;
 	imageViews.reserve(swapChainImages.size());
 	for (auto image : swapChainImages) {
-		vk::ImageViewCreateInfo imageViewCreateInfo(vk::ImageViewCreateFlags(), image, vk::ImageViewType::e2D, format, vk::ComponentMappin{ vk::ComponentSwizzle::eR, vk::ComponentSwizzle::eG, vk::ComponentSwizzle::eB, vk::ComponentSwizzle::eA }, vk::ImageSubresourceRange{ vk::ImageAspectFlagBits::eCoor, 0, 1, 0, 1 });
+		vk::ImageViewCreateInfo imageViewCreateInfo(vk::ImageViewCreateFlags(), image, vk::ImageViewType::e2D, format, vk::ComponentMapping{ vk::ComponentSwizzle::eR, vk::ComponentSwizzle::eG, vk::ComponentSwizzle::eB, vk::ComponentSwizzle::eA }, vk::ImageSubresourceRange{ vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1 });
 		imageViews.push_back(device->createImageViewUnique(imageViewCreateInfo));
 	}
 
@@ -109,20 +108,20 @@ int main() {
 		if (shaderModule.GetCompilationStatus() != shaderc_compilation_status_success) {
 			std::cerr << shaderModule.GetErrorMessage() << std::endl;
 		}
-		auto shaderCode = std:vector<uint32_t>{ shaderModule.cbegin(), shaderModule.cend() };
+		auto shaderCode = std::vector<uint32_t>{ shaderModule.cbegin(), shaderModule.cend() };
 		auto shaderSize = std::distance(shaderCode.begin(), shaderCode.end());
 		auto shaderCreateInfo = vk::ShaderModuleCreateInfo{ {}, shaderSize * sizeof(uint32_t), shaderCode.data() };
-		return divice->createShaderModuleUnique(shaderCreateInfo);
+		return device->createShaderModuleUnique(shaderCreateInfo);
 		};
 
 	auto vertexShaderModule = compileShader(vertexShader, shaderc_glsl_vertex_shader);
 	auto fragmentShaderModule = compileShader(fragmentShader, shaderc_glsl_fragment_shader);
-	auto vertShaderStageInfo = vk::PipelineShaderStageCreateInfo{ {}, vk::ShaderStageFLagBits::eVertex, *vertexShaderModule, "main" };
-	auto fragShaderStageInfo = vk::PipelineShaderStageCreateInfo{ {}, vk::ShaderStageFLagBits::eFragment, *fragmentShaderModule, "main" };
+	auto vertShaderStageInfo = vk::PipelineShaderStageCreateInfo{ {}, vk::ShaderStageFlagBits::eVertex, *vertexShaderModule, "main" };
+	auto fragShaderStageInfo = vk::PipelineShaderStageCreateInfo{ {}, vk::ShaderStageFlagBits::eFragment, *fragmentShaderModule, "main" };
 
 	auto pipelineShaderStages = std::vector<vk::PipelineShaderStageCreateInfo>{vertShaderStageInfo, fragShaderStageInfo};
 	
-	auto vertexInputInfo = vk::PipelineVertexInputStateCreateInfo{ {}, 0u, nullptr, 0u, mullptr };
+	auto vertexInputInfo = vk::PipelineVertexInputStateCreateInfo{ {}, 0u, nullptr, 0u, nullptr };
 
 	auto inputAssembly = vk::PipelineInputAssemblyStateCreateInfo{ {}, vk::PrimitiveTopology::eTriangleStrip, false };
 
@@ -149,7 +148,7 @@ int main() {
 
 	auto pipelineLayout = device->createPipelineLayoutUnique({}, nullptr);
 
-	auto colorAttachment = vk::AttachmentDescription{ {}, format, vk::SampleCountFlagBits::e1, vk::AttachmentLoardOp::eClear, vk::AttachmentStoreOp::eStore, {}, {}, {}, vk::ImageLayout::ePresentSrcKHR };
+	auto colorAttachment = vk::AttachmentDescription{ {}, format, vk::SampleCountFlagBits::e1, vk::AttachmentLoadOp::eClear, vk::AttachmentStoreOp::eStore, {}, {}, {}, vk::ImageLayout::ePresentSrcKHR };
 
 	auto colourAttachmentRef = vk::AttachmentReference{ 0, vk::ImageLayout::eColorAttachmentOptimal };
 
@@ -157,15 +156,15 @@ int main() {
 
 	auto semaphoreCreateInfo = vk::SemaphoreCreateInfo{};
 	auto imageAvailableSemaphore = device->createSemaphoreUnique(semaphoreCreateInfo);
-	auto renderFinishedSemaphore = device->createSemaphoreUnique(semaphoreCreateInfor);
+	auto renderFinishedSemaphore = device->createSemaphoreUnique(semaphoreCreateInfo);
 
-	auto subpassDependency = vk::SubpassDependency{ VK_SUBPASS_EXTERNAL, 0, vk::PipelineStageFlagBits::eColorAttachmentOutput, vk::PipelineStageFlagBits::eColorAttachmentOutput, {}, vk::accessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite };
+	auto subpassDependency = vk::SubpassDependency{ VK_SUBPASS_EXTERNAL, 0, vk::PipelineStageFlagBits::eColorAttachmentOutput, vk::PipelineStageFlagBits::eColorAttachmentOutput, {}, vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite };
 
 	auto renderPass = device->createRenderPassUnique(
 		vk::RenderPassCreateInfo{ {}, 1, &colorAttachment, 1, &subpass, 1, &subpassDependency }
 	);
 
-	auto pipelineCreateInfo = vk::GraphicsPipelineCreateInfo{ {}, 2, pipelineShaderStages.data(), &vertexInputInfo, &inputAssembly, nullpre, &viewportState, &rasterizer, nullptr, nullptr, &colorBlending, nullptr, *pipelineLayout, *renderPass, 0 };
+	auto pipelineCreateInfo = vk::GraphicsPipelineCreateInfo{ {}, 2, pipelineShaderStages.data(), &vertexInputInfo, &inputAssembly, nullptr, &viewportState, &rasterizer, nullptr, nullptr, &colorBlending, nullptr, *pipelineLayout, *renderPass, 0 };
 
 	auto pipeline = device->createGraphicsPipelineUnique({}, pipelineCreateInfo).value;
 
@@ -176,17 +175,17 @@ int main() {
 
 	auto commandPoolUnique = device->createCommandPoolUnique({ {}, static_cast<uint32_t>(presentQueueFamilyIndex) });
 
-	std::vector<vk::UniqueCommandBuffer> commandBuffers = device->allocateCommandBuffersUnique(vk::ComanndBufferAllocateInfo(
+	std::vector<vk::UniqueCommandBuffer> commandBuffers = device->allocateCommandBuffersUnique(vk::CommandBufferAllocateInfo(
 		commandPoolUnique.get(), vk::CommandBufferLevel::ePrimary, static_cast<uint32_t>(framebuffers.size())
 	));
 
 	auto deviceQueue = device->getQueue(static_cast<uint32_t>(presentQueueFamilyIndex), 0);
 
-	for (size_t i = 0; i < commandBuffers.size(), i++) {
+	for (size_t i = 0; i < commandBuffers.size(); i++) {
 		auto beginInfo = vk::CommandBufferBeginInfo{};
 		commandBuffers[i]->begin(beginInfo);
 		vk::ClearValue clearValues{};
-		auto renderPassBeginInfo = vk::RenderPassBeginInfo{ renderPass.get(), frameBuffers[i].get(), vk::Rect2D{ { 0, 0 }, vk::Extent2D{ windth, height }}, 1, &clearValues };
+		auto renderPassBeginInfo = vk::RenderPassBeginInfo{ renderPass.get(), framebuffers[i].get(), vk::Rect2D{ { 0, 0 }, vk::Extent2D{ width, height }}, 1, &clearValues };
 
 		commandBuffers[i]->beginRenderPass(renderPassBeginInfo, vk::SubpassContents::eInline);
 		commandBuffers[i]->bindPipeline(vk::PipelineBindPoint::eGraphics, *pipeline);
@@ -198,7 +197,7 @@ int main() {
 	while(!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		auto imageIndex = device->acquireNextImageKHR(swapChain.get(), std::numeric_limits<uint64_t>::max(), imageAvailableSemaphore.get(), {});
-		vk::PipelineStateFlags waitStageMash = vk::PipelineStageFlagBits::eColorAttachmentOutput;
+		vk::PipelineStageFlags waitStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput;
 		auto submitInfo = vk::SubmitInfo{ 1, &imageAvailableSemaphore.get(), &waitStageMask, 1, &commandBuffers[imageIndex.value].get(), 1, &renderFinishedSemaphore.get() };
 
 		deviceQueue.submit(submitInfo, {});
