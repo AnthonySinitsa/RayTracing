3974af3d9eea0288728b03f6c21298e9775ddf4e
AnthonySinitsa <itonyemail@gmail.com>
2024-07-25 23:20:29 +0000

    fixed variable assignment bug

--- RayTracing/lve_pipeline.cpp
+++ RayTracing/lve_pipeline.cpp
@@ -79,7 +79,7 @@ namespace lve {
 		VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
 		vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
 		vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
-		vertexInputInfo.vertexBindingDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+		vertexInputInfo.vertexBindingDescriptionCount = static_cast<uint32_t>(bindingDescriptions.size());
 		vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
 		vertexInputInfo.pVertexBindingDescriptions = bindingDescriptions.data();
 
