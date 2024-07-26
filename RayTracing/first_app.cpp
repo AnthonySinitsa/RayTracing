9e0e43e9e8fd8fe4bb7694630e8d3063fcedd184
AnthonySinitsa <itonyemail@gmail.com>
2024-07-26 07:50:18 +0000

    messing around with animation of said triangle

--- RayTracing/first_app.cpp
+++ RayTracing/first_app.cpp
@@ -123,6 +123,9 @@ namespace lve {
 	}
 
 	void FirstApp::recordCommandBuffer(int imageIndex) {
+		static int frame = 0;
+		frame = (frame + 1) % 10000;
+
 		VkCommandBufferBeginInfo beginInfo{};
 		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -139,7 +142,7 @@ namespace lve {
 		renderPassInfo.renderArea.extent = lveSwapChain->getSwapChainExtent();
 
 		std::array<VkClearValue, 2> clearValues{};
-		clearValues[0].color = { 0.1f, 0.1f, 0.1f, 1.0f };
+		clearValues[0].color = { 0.01f, 0.01f, 0.01f, 1.0f };
 		clearValues[1].depthStencil = { 1.0f, 0 };
 
 		renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
@@ -163,7 +166,7 @@ namespace lve {
 
 		for (int j = 0; j < 4; j++) {
 			SimplePushConstantData push{};
-			push.offset = { 0.0f, -0.4f + j * 0.25f };
+			push.offset = { -0.5f + frame * 0.0002f, -0.4f + j * 0.25f };
 			push.color = { 0.0f, 0.0f, 0.2f + 0.2f * j };
 
 			vkCmdPushConstants(
