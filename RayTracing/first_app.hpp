d0707506ab8bb79267d4bb1161e012ab73f5d911
AnthonySinitsa <itonyemail@gmail.com>
2024-07-27 07:01:46 +0000

    cleanup app implement

--- RayTracing/first_app.hpp
+++ RayTracing/first_app.hpp
@@ -2,7 +2,6 @@
 
 #include "lve_device.hpp"
 #include "lve_game_object.hpp"
-#include "lve_pipeline.hpp"
 #include "lve_renderer.hpp"
 #include "lve_window.hpp"
 
@@ -27,15 +26,11 @@ namespace lve {
 
 	private:
 		void loadGameObjects();
-		void createPipelineLayout();
-		void createPipeline();
-		void renderGameObjects(VkCommandBuffer commandBuffer);
 
 		LveWindow lveWindow{ WIDTH, HEIGHT, "Hello Vulkan!" };
 		LveDevice lveDevice{lveWindow};
 		LveRenderer lveRenderer{ lveWindow, lveDevice };
-		std::unique_ptr<LvePipeline> lvePipeline;
-		VkPipelineLayout pipelineLayout;
+
 		std::vector<LveGameObject> gameObjects;
 	};
 }
