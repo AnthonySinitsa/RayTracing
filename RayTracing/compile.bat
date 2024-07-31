ffd268451d99d094438c5fee30caa33e0e40905a
AnthonySinitsa <itonyemail@gmail.com>
2024-07-31 07:24:23 +0000

    typo fix

--- RayTracing/compile.bat
+++ RayTracing/compile.bat
@@ -1,3 +1,5 @@
 C:\VulkanSDK\1.3.283.0\Bin\glslangValidator.exe -V simple_shader.vert -o simple_shader.vert.spv
 C:\VulkanSDK\1.3.283.0\Bin\glslangValidator.exe -V simple_shader.frag -o simple_shader.frag.spv
+C:\VulkanSDK\1.3.283.0\Bin\glslangValidator.exe -V point_light.frag -o point_light.frag.spv
+C:\VulkanSDK\1.3.283.0\Bin\glslangValidator.exe -V point_light.frag -o point_light.frag.spv
 pause
--- RayTracing/point_light.frag
+++ RayTracing/point_light.frag
@@ -1,7 +1,7 @@
 #version 450
 
-layout(location = 0) in vec2 fragOffset;
-layout(location = 0) in vec4 outColor;
+layout (location = 0) in vec2 fragOffset;
+layout (location = 0) out vec4 outColor;
 
 layout(set = 0, binding = 0) uniform GlobalUbo {
 	mat4 projection;
